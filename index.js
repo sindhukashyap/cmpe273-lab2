21b2d68de1efc5c99e6e5e51d4afac97ed72b481
sindhu_nk <sindhunkashyap@gmail.com>
2014-09-17 00:44:12 -0700

    cmp-lab2: implement simple cookie-based authentication application using node.js and connect middleware framework

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var connect = require('connect');
 var login = require('./login');
-
+var http = require('http'); 
 var app = connect();
+
 
 app.use(connect.json()); // Parse JSON request body into `request.body`
 app.use(connect.urlencoded()); // Parse form in request body into `request.body`
 app.use(connect.cookieParser()); // Parse cookies in the request headers into `request.cookies`
 app.use(connect.query()); // Parse query string into `request.query`
 
-app.use('/', main);
+app.use('/', main); //function main intersepts every request at path '/' of server
+
 
 function main(request, response, next) {
 	switch (request.method) {
@@ -19,46 +21,78 @@ function main(request, response, next) {
 	}
 };
 
-function get(request, response) {
+function get(request, response) 
+{
 	var cookies = request.cookies;
 	console.log(cookies);
-	if ('session_id' in cookies) {
+	if ('session_id' in cookies) 
+	{
 		var sid = cookies['session_id'];
-		if ( login.isLoggedIn(sid) ) {
+		if ( login.isLoggedIn(sid) ) 
+		{
+			
 			response.setHeader('Set-Cookie', 'session_id=' + sid);
+			response.setHeader("Content-Type", "text/html");
 			response.end(login.hello(sid));	
-		} else {
+		} else
+		 {
 			response.end("Invalid session_id! Please login again\n");
 		}
-	} else {
+	} 
+	else 
+	{
 		response.end("Please login via HTTP POST\n");
 	}
 };
 
-function post(request, response) {
-	// TODO: read 'name and email from the request.body'
-	// var newSessionId = login.login('xxx', 'xxx@gmail.com');
-	// TODO: set new session id to the 'session_id' cookie in the response
-	// replace "Logged In" response with response.end(login.hello(newSessionId));
+function post(request, response) 
+{
+	// read 'name and email from the request.body'
+	var name = request.body.name;
+	var email = request.body.email;
 
-	response.end("Logged In\n");
+	var newSessionId = login.login(name,email);
+	var cookies = request.cookies;
+	// set new session id to the 'session_id' cookie in the response
+	response.setHeader('Set-Cookie','session_id =' + newSessionId);
+	response.setHeader("Content-Type", "text/html");
+	response.end(login.hello(newSessionId));
+};
+
+function put(request, response) 
+{
+	console.log("PUT:: Re-generate new seesion_id for the same user");
+	//  refresh session id; similar to the post() function
+	
+	var cookies = request.cookies;
+	console.log(cookies);
+	if ('session_id' in cookies) 
+	{
+		var newSessionId = cookies['session_id'];
+	}
+	response.setHeader('Set-Cookie','seesion_id =' + newSessionId);
+	response.setHeader("Content-Type", "text/html");
+	response.end(login.hello(newSessionId));
+	response.end("Re-freshed session id\n");
 };
 
 function del(request, response) {
 	console.log("DELETE:: Logout from the server");
- 	// TODO: remove session id via login.logout(xxx)
- 	// No need to set session id in the response cookies since you just logged out!
+	var cookies = request.cookies;
+	console.log(cookies);
+	if ('session_id' in cookies) 
+	{
+		var sid = cookies['session_id'];
+	}
+	login.logout(sid);
 
+ 	// remove session id via login.logout(xxx)
+ 	// No need to set session id in the response cookies since you just logged out!
+ 	response.setHeader("Content-Type", "text/html");
   	response.end('Logged out from the server\n');
 };
-
-function put(request, response) {
-	console.log("PUT:: Re-generate new seesion_id for the same user");
-	// TODO: refresh session id; similar to the post() function
-
-	response.end("Re-freshed session id\n");
-};
-
-app.listen(8000);
-
+//creating a server and making it ready to listen
+http.createServer(app).listen(8000);
 console.log("Node.JS server running at 8000...");
+
+
